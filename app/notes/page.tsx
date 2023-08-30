d23e4aec94c35d3eb8972f8ef183579119095560
Shakeel Haider <shkhaider2015@gmail.com>
2023-08-30 09:22:43 +0500

    Added notes to frontend

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,37 +1,45 @@
 import { INotes } from "@/interfaces";
 import { Constants } from "@/utils"
 import axios from "axios"
+import styles from './notes.module.css';
 
 async function getNotes() {
-    let base_url:string = Constants.getBaseUrl();
-    const {data} = await axios.get(base_url)
-    // console.log("Data : ", data)
-    return "What"
+    let base_url:string = Constants.getAPIUrl();
+    const {data} = await axios.get(base_url+'notes')
+    console.log("Data : ", data)
+    return {
+        data: data.data || [],
+        message: data.message || ""
+    }
 }
 
 export default async function NotesPage() {
 
     const data = await getNotes()
 
-    return <div>
+    return <div  >
         <h1>Notes</h1>
-        <Note />
+        <div className={styles.noteContainer}>
+        {
+            data.data?.map((item:any) =>  <Note title={item?.title} details={item?.details} />)
+        }
+        </div>
+        
     </div>
     
 }
 
-const Note = () => {
-    return <div className="note-item" >
-        <legend className="title" >Title</legend>
-        <span className="details" >
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-            Lorem Ipsum Dolar sit amet
-        </span>
+
+const Note = (props:INote) => {
+    const { title, details } = props;
+
+    return <div className={styles.noteItem} >
+        <legend className={styles.title} >{title}</legend>
+        <span className={styles.details} >{details}</span>
     </div>
+}
+
+interface INote {
+    title: string;
+    details: string;
 }
--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -2,12 +2,17 @@ const devURL = 'http://localhost:3000/';
 const prodURL = 'https://nextjswithtypescript-three.vercel.app/';
 
 class Constants {
-    private DEV_URL:string =  'http://localhost:3000/';
-    private PROD_URL:string = 'http://localhost:3000/';
+    private DEV_URL:string =  'http://localhost:3011/';
+    private PROD_URL:string = 'http://localhost:3011/';
     
     public getBaseUrl() {
         if(process.env.NODE_ENV === 'production') return this.PROD_URL;
-        else return this.DEV_URL
+        else return this.DEV_URL;
+    }
+
+    public getAPIUrl(){
+        if(process.env.NODE_ENV === 'production') return this.PROD_URL+'api/';
+        else return this.DEV_URL+'api/';
     }
 }
 
