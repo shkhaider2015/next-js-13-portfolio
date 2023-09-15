740b516c4f0d787985fc7de1800be5267237c00c
Shakeel Haider <shkhaider2015@gmail.com>
2023-09-15 23:06:03 +0500

    Add navbar

--- app/_components/Navbar/index.tsx
+++ app/_components/Navbar/index.tsx
@@ -1,18 +1,69 @@
-import Link from "next/link";
+// import Link from "next/link";
 import styles from "./Navbar.module.css";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
     <section className={styles.navContainer}>
-     <nav className={styles.stroke}>
-    <ul>
-      <li><Link href="#">Home</Link></li>
-      <li><Link href="#">About</Link></li>
-      <li><Link href="#">Downloads</Link></li>
-      <li><Link href="#">More</Link></li>
-      <li><Link href="#">Nice staff</Link></li>
-    </ul>
-  </nav>
+      <nav className={styles.fill}>
+        <ul>
+          <li>
+            <Link
+              activeClass="active"
+              to="screen1"
+              spy={true}
+              smooth={true}
+              offset={0}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="screen2"
+              spy={true}
+              smooth={true}
+              offset={0}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="screen3"
+              spy={true}
+              smooth={true}
+              offset={0}
+            >
+              Downloads
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="screen4"
+              spy={true}
+              smooth={true}
+              offset={0}
+            >
+              More
+            </Link>
+          </li>
+          <li>
+            <Link
+              activeClass="active"
+              to="screen5"
+              spy={true}
+              smooth={true}
+              offset={0}
+            >
+              Nice staff
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </section>
   );
 };
