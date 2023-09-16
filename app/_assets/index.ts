74174cd17c8425b56b643681ec60a6f8dbb694b1
Shakeel Haider <shkhaider2015@gmail.com>
2023-09-16 17:17:54 +0500

    added animating typing

---
+++ app/_assets/index.ts
@@ -0,0 +1,9 @@
+"use client"
+
+import MyImage from './my-pic-1.png';
+import MyImage2 from './my-pic-2.png';
+
+export {
+    MyImage,
+    MyImage2
+}
--- app/_components/LandingScreen/screens/screen1/index.tsx
+++ app/_components/LandingScreen/screens/screen1/index.tsx
@@ -1,9 +1,33 @@
 import { Element } from 'react-scroll';
 import styles from './screen1.module.css';
+import Image from 'next/image';
+import { MyImage, MyImage2 } from '@/app/_assets';
+import { TypeAnimation } from 'react-type-animation';
 
 const Screen1 = () => {
     return <Element name="screen1" className={styles.container}>
-    <h1>Screen One</h1>
+    <div className={styles.left} >
+      <Image src={MyImage2} alt='' className={styles.image} width={300} />
+    </div>
+    <div className={styles.right}>
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Full Stack Development',
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        'Frontend Development',
+        1000,
+        'Backend Development',
+        1000,
+        'Machin Learning',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '2em', display: 'inline-block', opacity: .5 }}
+      repeat={Infinity}
+    />
+    </div>
   </Element>
 }
 
