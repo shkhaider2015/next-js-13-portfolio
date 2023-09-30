00b36e115bb971a5b134c46c5200d9f86b4b1855
Shakeel Haider <shkhaider2015@gmail.com>
2023-09-30 16:00:40 +0500

    add custom type

--- app/_components/LandingScreen/screens/screen1/index.tsx
+++ app/_components/LandingScreen/screens/screen1/index.tsx
@@ -2,7 +2,7 @@ import { Element } from 'react-scroll';
 import styles from './screen1.module.css';
 import Image from 'next/image';
 import { MyImage, MyImage2, MyImage3, MyImage4 } from '@/app/_assets';
-import { TypeAnimation } from 'react-type-animation';
+import TypeWriter from '@/app/_components/TypeWriter';
 
 const Screen1 = () => {
     return <Element name="screen1" className={styles.container}>
@@ -10,23 +10,7 @@ const Screen1 = () => {
       <Image src={MyImage4} alt='' className={styles.image} width={300} />
     </div>
     <div className={styles.right}>
-    <TypeAnimation
-      sequence={[
-        // Same substring at the start will only be typed out once, initially
-        'Full Stack Development',
-        1000, // wait 1s before replacing "Mice" with "Hamsters"
-        'Frontend Development',
-        1000,
-        'Backend Development',
-        1000,
-        'Machin Learning',
-        1000
-      ]}
-      wrapper="span"
-      speed={50}
-      style={{ fontSize: '2em', display: 'inline-block', opacity: .5 }}
-      repeat={Infinity}
-    />
+    <TypeWriter texts={['My Name ', 'My Owl One ', "My own"]} infinite={true} /> 
     </div>
   </Element>
 }
