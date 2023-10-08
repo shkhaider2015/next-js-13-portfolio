45c9bbbe98cff37e9943362f09728f760d3d0aa1
Shakeel Haider <shkhaider2015@gmail.com>
2023-10-08 16:15:18 +0500

    add bg image screen1

--- app/_assets/index.ts
+++ app/_assets/index.ts
@@ -4,10 +4,14 @@ import MyImage from './my-pic-1.png';
 import MyImage2 from './my-pic-2.png';
 import MyImage3 from './my-pic-3.png';
 import MyImage4 from './my-pic-4.png';
+import Screen1_back_animation from "./screen1-back.json";
+import bg_pattern from "./BG-25.png";
 
 export {
     MyImage,
     MyImage2,
     MyImage3,
-    MyImage4
+    MyImage4,
+    Screen1_back_animation,
+    bg_pattern
 }
--- app/_components/LandingScreen/screens/screen1/index.tsx
+++ app/_components/LandingScreen/screens/screen1/index.tsx
@@ -1,11 +1,13 @@
 import { Element } from 'react-scroll';
 import styles from './screen1.module.css';
 import Image from 'next/image';
-import { MyImage, MyImage2, MyImage3, MyImage4 } from '@/app/_assets';
+import {MyImage4, bg_pattern } from '@/app/_assets';
 import TypeWriter from '@/app/_components/TypeWriter';
+import Lottie from 'react-lottie';
 
 const Screen1 = () => {
     return <Element name="screen1" className={styles.container}>
+      <Image src={bg_pattern} alt='bg-image' className={styles.bgImage} />
     <div className={styles.left} >
       <Image src={MyImage4} alt='' className={styles.image} width={300} />
     </div>
